4d7b2b67fbc8d9f96358e2135dc1e98e1915087e
nakaearth <naka5313@gmail.com>
2015-04-13 21:42:02 +0900

    修正

--- recipes/nginx/nginx.rb
+++ recipes/nginx/nginx.rb
@@ -2,10 +2,24 @@ package 'nginx' do
   action :install
 end
 
+directory "/etc/nginx" do
+  action :create # デフォルトのため記載不要だが書いたほうが可読性が高い
+  mode "755" # 任意指定
+  owner "vagrant" # 任意指定
+  group "vagrant" # 任意指定
+end
+
+directory "/var/log/nginx" do
+  action :create # デフォルトのため記載不要だが書いたほうが可読性が高い
+  mode "755" # 任意指定
+  owner "vagrant" # 任意指定
+  group "vagrant" # 任意指定
+end
+
 template "/etc/nginx/nginx.conf" do 
- source "../../template/nginx/nginx_conf.rb"
- # path ""
- # variables({"param1" => "value1"}) # 任意指定。
+  source "../../template/nginx/nginx_conf.rb"
+  # path ""
+  variables({"error_log_path" => "/var/log/nginx/app_error.log"})
 end
 
 service 'nginx' do
--- template/nginx/nginx_conf.rb
+++ template/nginx/nginx_conf.rb
@@ -1,10 +1,8 @@
-ser nginx;
+user nginx;
 worker_processes 1;
-error_log /var/log/nginx/error.log;
-pid /var/runfer_name localhost;
-    location / {
-      root /usr/share/nginx/html;
-      index index.html index.htm;
-    }
-  }
+error_log <%= @error_log_path %>;
+pid  /var/run/nginx.pid;
+events {
+    worker_connections  1024;
+    multi_accept on;
 }
