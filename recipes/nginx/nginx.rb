009d3b08ebe393258baa65db4ee332f5077503c7
nakaearth <naka5313@gmail.com>
2015-04-15 19:29:42 +0900

    nodeを追加

--- recipes/nginx/nginx.rb
+++ recipes/nginx/nginx.rb
@@ -19,7 +19,7 @@ end
 template "/etc/nginx/nginx.conf" do 
   source "../../template/nginx/nginx_conf.rb"
   # path ""
-  variables({"error_log_path" => "/var/log/nginx/app_error.log"})
+  variables({"error_log_path" => "#{node["error_log_path"]}"})
 end
 
 service 'nginx' do
