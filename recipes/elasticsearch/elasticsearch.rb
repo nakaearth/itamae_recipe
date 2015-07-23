b1059e9351bfb657b4eb99e66782dfe59d9ae874
nakaearth <naka5313@gmail.com>
2015-07-23 19:59:57 +0900

    修正

--- recipes/elasticsearch/elasticsearch.rb
+++ recipes/elasticsearch/elasticsearch.rb
@@ -1,9 +1,18 @@
-package "install java" do
-  name "java"
-  action :install
+%w( yum java-1.8.0-openjdk ).each do |pkg|
+  package pkg do
+    action :install
+  end
 end
 
-package "install elasticsearch" do
-  name "elasticsearch"
+file "/etc/yum.repos.d/elasticsearch.reop" do
+  content "[elasticsearch-1.0]\n
+           name=Elasticsearch repository for 1.0.x packages\n
+           baseurl=http://packages.elasticsearch.org/elasticsearch/1.0/centos\n
+           gpgcheck=1\n
+           $gpgkey=http://packages.elasticsearch.org/GPG-KEY-elasticsearch\n
+           enabled=1\n"
+end
+
+package 'elasticsearch' do
   action :install
 end
---
+++ recipes/java/java.rb
@@ -0,0 +1,3 @@
+package 'java-1.8.0-openjdk' do
+  action :install
+end
