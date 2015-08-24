fcd1bb35ce547f512f89aedfa76a7c3efa3c0994
nakamura shinichirou <naka5313@gmail.com>
2015-08-25 03:28:48 +0900

    テンプレート作成途中

--- template/elasticsearch/conf/elasticsearch_yml.rb
+++ template/elasticsearch/conf/elasticsearch_yml.rb
@@ -1,6 +1,6 @@
-cluster.name: nakamura-elasticsearch
+<%= "cluster.name: nakamura-elasticsearch" %>
 
-index.number_of_shards: 5
-index.numbber_of_replicas: 1
+<%= "index.number_of_shards: #{node['index_shards_num']}" %>
+<%= "index.number_of_replicas: #{node['index_replicas_num']}" %>
 
-network.host: '_eth1:ipv4_'
+<%= "network.host: '_eth1:ipv4_'" %>
