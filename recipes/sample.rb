641a1c197644014841203f047033d638b1dd50d8
nakamura shinichirou <naka5313@gmail.com>
2015-09-21 07:43:58 +0900

    追加

--- recipes/sample.rb
+++ recipes/sample.rb
@@ -1,9 +1,26 @@
 execute 'echo hello' do
   user 'vagrant'
-  command "mkdir ~/test_dir"
+  command "echo 'hoge'"
 end
 
 execute "update yum repo" do
   user "root"
   command "yum -y update"
 end
+
+directory "/usr/local/test" do
+  action :create
+  mode '777'
+  owner 'vagrant'
+  group 'vagrant'
+end
+
+
+file "test" do
+  content 'ほげ'
+  mode '777'
+  owner 'vagrant'
+  group 'vagrant'
+end
+
+
