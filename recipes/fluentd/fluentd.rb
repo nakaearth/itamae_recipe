663c578120ed0f016d4f2df667e52266e3f817b1
nakamura shinichirou <naka5313@gmail.com>
2015-11-04 23:39:04 +0900

    試しに追加

--- recipes/fluentd/fluentd.rb
+++ recipes/fluentd/fluentd.rb
@@ -1,5 +1,9 @@
 # rubyをinstall
 include_recipe '../../recipes/ruby_dev_env/ruby_dev_env.rb'
 
-execute 'gem install fluentd --no-ri --no-rdoc'
+# fluentd のプラグイン
+fluent_plugins = %w( fluent-plugin-slack fluent-plugin-elasticsearch )
+fluent_plugins.each do |plugin|
+  execute "fluent-gem install '#{plugin}'"
+end
 
