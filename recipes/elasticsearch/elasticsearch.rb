cebcbf50f57e744abcb5698ecfa865b913ed091b
nakaearth <naka5313@gmail.com>
2015-11-09 19:40:24 +0900

    追加です

--- recipes/elasticsearch/elasticsearch.rb
+++ recipes/elasticsearch/elasticsearch.rb
@@ -70,4 +70,40 @@ if  /\A1.*/ =~ node[:elasticsearch][:version]
   execute "bin/plugin -install elasticsearch/elasticsearch-analysis-kuromoji/#{node['elasticsearch']['plugin']['kuromoji']['version']}" do
     cwd '/usr/local/share/elasticsearch'
   end
+else
+  execute "sudo chmod -R 777 /usr/local/share/elasticsearch" do
+    only_if 'cd /usr/local/share/elasticsearch/'
+    cwd '/usr/local/share/elasticsearch'
+  end
+
+  # プラグイン
+  ## HEAD
+  execute 'bin/plugin remove mobz/elasticsearch-head' do
+    only_if 'cd /usr/local/share/elasticsearch/plugins/head'
+    cwd '/usr/local/share/elasticsearch'
+  end
+
+  execute 'bin/plugin install mobz/elasticsearch-head' do
+    cwd '/usr/local/share/elasticsearch'
+  end
+
+  ## kuromoji
+  execute "bin/plugin remove analysis-kuromoji" do
+    only_if 'cd /usr/local/share/elasticsearch/plugins/analysis-kuromoji'
+    cwd '/usr/local/share/elasticsearch'
+  end
+
+  execute "bin/plugin install analysis-kuromoji" do
+    cwd '/usr/local/share/elasticsearch'
+  end
+
+  template "/usr/local/share/elasticsearch/config/elasticsearch.yml" do 
+    path "/usr/local/share/elasticsearch/config/elasticsearch.yml" # 任意指定。ここに記載するとブロック引数より優先される。
+    source "../../templates/elasticsearch/config/elasticsearch2_yml.erb" #必須指定。
+    variables({cluster_name: 'nakamura-elasticsearch', index_shards_num: "5", index_replicas_num: "1"}) # 任意指定。
+  end
+
+  execute "bin/elasticsearch -d" do
+    cwd '/usr/local/share/elasticsearch'
+  end
 end
