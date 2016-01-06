1aed9df4199049533a6bf87f53dfe850fabf1f0a
nakaearth <naka5313@gmail.com>
2016-01-06 21:31:01 +0900

    レシピを修正

--- recipes/elasticsearch/version/2_x.rb
+++ recipes/elasticsearch/version/2_x.rb
@@ -30,88 +30,88 @@ execute 'kinaba unzip' do
   command 'tar -zxvf kibana.tar.gz'
 end
 
-execute 'sudo rm -R /usr/local/share/elasticsearch' do
+execute 'rm -R elasticsearch' do
   only_if 'ls /usr/local/share/elasticsearch'
 end
 
-execute 'sudo mv elasticsearch-* /usr/local/share/elasticsearch' do
-  not_if 'ls /usr/local/share/elasticsearch'
+execute 'mv elasticsearch-* elasticsearch' do
+  not_if 'ls elasticsearch'
 end
 
 # kibana
-execute 'sudo rm -R /usr/local/share/kibana' do
-  only_if 'ls /usr/local/share/kibana'
+execute 'rm -R kibana' do
+  only_if 'ls kibana'
 end
 
-execute 'sudo mv kibana-* /usr/local/share/kibana' do
-  not_if 'ls /usr/local/share/kibana'
+execute 'mv kibana-* kibana' do
+  not_if 'ls kibana'
 end
 
-execute 'sudo chmod -R 775 /usr/local/share/elasticsearch' do
-  only_if 'ls /usr/local/share/elasticsearch'
+execute 'chmod -R 775 elasticsearch' do
+  only_if 'ls elasticsearch'
 end
 
 # kibana
-execute 'sudo chmod -R 775 /usr/local/share/kibana' do
-  only_if 'ls /usr/local/share/kibana'
+execute 'chmod -R 775 kibana' do
+  only_if 'ls kibana'
 end
 
-execute 'mkdir /usr/local/share/elasticsearch/plugins' do
-  not_if 'ls /usr/local/share/elasticsearch/plugins'
+execute 'mkdir elasticsearch/plugins' do
+  not_if 'ls elasticsearch/plugins'
 end
 
 # プラグイン
 ## HEAD
 execute 'bin/plugin remove mobz/elasticsearch-head' do
-  only_if 'cd /usr/local/share/elasticsearch/plugins/head'
-  cwd '/usr/local/share/elasticsearch'
+  only_if 'cd elasticsearch/plugins/head'
+  cwd 'elasticsearch'
 end
 
 execute 'bin/plugin install mobz/elasticsearch-head' do
-  cwd '/usr/local/share/elasticsearch'
+  cwd 'elasticsearch'
 end
 
 ## kuromoji
 execute "bin/plugin remove analysis-kuromoji" do
-  only_if 'cd /usr/local/share/elasticsearch/plugins/analysis-kuromoji'
-  cwd '/usr/local/share/elasticsearch'
+  only_if 'cd elasticsearch/plugins/analysis-kuromoji'
+  cwd 'elasticsearch'
 end
 
 execute "bin/plugin install analysis-kuromoji" do
-  cwd '/usr/local/share/elasticsearch'
+  cwd 'elasticsearch'
 end
 
 # mavel
- execute "bin/plugin remove license" do
-  only_if 'cd /usr/local/share/elasticsearch/plugins/license'
-  cwd '/usr/local/share/elasticsearch'
-end
+# execute "bin/plugin remove license" do
+#  only_if 'cd elasticsearch/plugins/license'
+#  cwd 'elasticsearch'
+#end
 
-execute "bin/plugin install license" do
-  cwd '/usr/local/share/elasticsearch'
-end
-
-execute "bin/plugin remove marvel-agent" do
-  only_if 'cd /usr/local/share/elasticsearch/plugins/marvel-agent'
-  cwd '/usr/local/share/elasticsearch'
-end
-
-execute "bin/plugin install marvel-agent" do
-  cwd '/usr/local/share/elasticsearch'
-end
+#execute "bin/plugin install license" do
+#  cwd 'elasticsearch'
+#end
+#
+#execute "bin/plugin remove marvel-agent" do
+#  only_if 'cd elasticsearch/plugins/marvel-agent'
+#  cwd 'elasticsearch'
+#end
+#
+#execute "bin/plugin install marvel-agent" do
+#  cwd 'elasticsearch'
+##end
 
 # marvel plugin
-execute './bin/kibana plugin -i elasticsearch/marvel-ui/latest' do
-  cwd '/usr/local/share/kibana'
-end
+#execute './bin/kibana plugin -i elasticsearch/marvel-ui/latest' do
+#  cwd 'kibana'
+#end
 
 # elasticsearch.yml
-template "/usr/local/share/elasticsearch/config/elasticsearch.yml" do
-  path "/usr/local/share/elasticsearch/config/elasticsearch.yml" # 任意指定。ここに記載するとブロック引数より優先される。
+template "elasticsearch/config/elasticsearch.yml" do
+  path "elasticsearch/config/elasticsearch.yml" # 任意指定。ここに記載するとブロック引数より優先される。
   source "../../../templates/elasticsearch/config/elasticsearch2_yml.erb" #必須指定。
   variables({cluster_name: 'nakamura-elasticsearch', index_shards_num: "5", index_replicas_num: "1"}) # 任意指定。
 end
 
-#execute "bin/elasticsearch -d" do
-#  cwd '/usr/local/share/elasticsearch'
-#end
+execute "bin/elasticsearch -d" do
+  cwd '/usr/local/share/elasticsearch'
+end
